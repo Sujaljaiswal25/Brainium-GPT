db29231473147cc6ffa8bf59f06795c187c21fdf
Sujaljaiswal25 <sujaljaiswal304@gmail.com>
2025-08-17 12:50:29 +0530

    setup Socket.io

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/db/db")
+const initSocketServer = require("./src/sockets/socket.server")
+const httpServer = require("http").createServer(app)
 
 
 connectToDB();
 
+initSocketServer(httpServer)
 
 
 
 
 
 
-app.listen(3000, ()=>{
+httpServer.listen(3000, ()=>{
     console.log("server start on port no 3000")
 })
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const authRoute = require("./routes/auth.route")
+const chatRoute = require("./routes/chat.route")
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute)
+app.use("/api/chat", chatRoute)
 
 module.exports = app;
